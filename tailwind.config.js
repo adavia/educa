ec398498fb6feb067c0cd5400aa15862821a3720
adavia <andresi.davia@gmail.com>
2019-09-02 18:17:43 -0500

    Create class base views to manage courses

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  theme: {
-    fontFamily: {
-			'body': ['Source Sans Pro', 'sans-serif']
-		},
-    extend: {}
-  },
-  variants: {},
-  plugins: []
+    theme: {
+        fontFamily: {
+            'body': ['Source Sans Pro', 'sans-serif']
+        },
+        extend: {}
+    },
+    variants: {
+        borderWidth: ['responsive', 'last', 'hover', 'focus'],
+        padding: ['responsive', 'last', 'hover', 'focus'],
+        margin: ['responsive', 'last', 'hover', 'focus']
+    },
+    plugins: []
 }
